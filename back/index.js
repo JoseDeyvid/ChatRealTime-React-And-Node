5cb1286e7b5af08b5922dfbfb4419aa9605aac35
Jose Deyvid <j_deyvid@live.com>
2024-10-26 11:09:22 -0300

    Conectar com o socket do lado do server

--- back/index.js
+++ back/index.js
@@ -4,4 +4,17 @@ const io = require("socket.io")(server, {cors: {origin: "http://localhost:5173"}
 
 const PORT = 3001;
 
+io.on("connect", socket => {
+    console.log("Usuário conectado: ", socket.id)
+
+    socket.on('disconnect', reason => {
+        console.log("Usuário desconectado: ", reason)
+    })
+
+    socket.on('set_username', username => {
+        socket.data.username = username
+    })
+
+})
+
 server.listen(PORT, () => console.log("Server running..."))
